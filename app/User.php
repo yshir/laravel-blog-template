fd15091c6523950379f507962ef82daf82763f67
yshir <yshir0020@gmail.com>
2018-11-12 21:33:34 +0900

    add fillable to User model

--- app/User.php
+++ app/User.php
@@ -11,7 +11,7 @@ class User extends Authenticatable
     use Notifiable;
 
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'avatar', 'role', 'email', 'password',
     ];
 
     protected $hidden = [
