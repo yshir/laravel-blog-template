6c9b24ab462a1ae53dc7770366cf637c68dce394
yshir <yshir0020@gmail.com>
2018-11-13 21:35:07 +0900

    guard routes.web

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 // auth
-Auth::routes();
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
 Route::get('/home', 'HomeController@index')->name('home');
 
 // posts
@@ -9,3 +11,23 @@ Route::get('/', 'PostController@index')->name('posts.index');
 Route::get('/categories/{slug}', 'PostController@category')->name('posts.category');
 Route::get('/tags/{slug}', 'PostController@tag')->name('posts.tag');
 Route::get('/post/{slug}', 'PostController@show')->name('posts.show');
+
+// editor-higher
+Route::group(['middleware' => ['auth', 'can:editor-higher']], function () {
+  
+});
+
+// user-higher
+Route::group(['middleware' => ['auth', 'can:user-higher']], function () {
+  
+});
+
+// admin-higher
+Route::group(['middleware' => ['auth', 'can:admin-higher']], function () {
+  
+});
+
+// system-higher
+Route::group(['middleware' => ['auth', 'can:system-higher']], function () {
+  
+});
